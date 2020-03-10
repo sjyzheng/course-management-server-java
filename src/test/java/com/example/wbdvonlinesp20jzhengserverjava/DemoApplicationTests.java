db7e841134d11a5464cd7ad867c599d42a6208db
sjyzheng <zjysteve@gmail.com>
2020-03-10 17:26:59 -0600

    Initial commit

---
+++ src/test/java/com/example/wbdvonlinesp20jzhengserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvonlinesp20jzhengserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
