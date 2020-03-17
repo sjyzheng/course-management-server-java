3e86f138f91283ff2ee34ae05e6d327ea6567327
sjyzheng <zjysteve@gmail.com>
2020-03-16 20:32:44 -0700

    Create Course.java

---
+++ src/main/java/com/example/wbdvonlinesp20jzhengserverjava/models/Course.java
@@ -0,0 +1,59 @@
+package com.example.wbdvonlinesp20jzhengserverjava.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title = "New Course";
+    private String dateCreated;
+    private String dateModified;
+
+    @OneToMany(mappedBy = "course", orphanRemoval = true)
+    private List<Module> modules;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(String dateModified) {
+        this.dateModified = dateModified;
+    }
+
+    public List<Module> getModules() {
+        return modules;
+    }
+
+    public void setModules(List<Module> modules) {
+        this.modules = modules;
+    }
+}
